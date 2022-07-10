ca3fed6929b0798283523fba9174d3ce2c26c2b6
Mihir <20bcs131@iiitdmj.ac.in>
2022-07-10 14:42:38 +0530

    update

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True  
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://blog-site-d.herokuapp.com/']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 #path of folder where to store uploaded file
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
